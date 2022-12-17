75c9bff87dbd4de49f355dfe9fd532345975e029
hk2511 <strapkobrest@gmail.com>
2022-12-17 19:08:33 +0300

    init shop entity

---
+++ src/app/shop/detail-shop.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-shop',
+  templateUrl: './detail-shop.component.html',
+  styleUrls: ['./detail-shop.component.scss']
+})
+export class DetailShopComponent {
+
+}
---
+++ src/app/shop/update-shop.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-shop',
+  templateUrl: './update-shop.component.html',
+  styleUrls: ['./update-shop.component.scss']
+})
+export class UpdateShopComponent {
+
+}
